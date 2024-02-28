58dbefe55f9d644da6114240522ecaaf510472e2
Vladimír Ulman <ulman@fi.muni.cz>
2024-02-28 17:35:34 +0100

    ADD: demo-ability to run headless

--- mastodon/src/main/java/org/ulman/simulator/ui/SimulatorIJ2UI.java
+++ mastodon/src/main/java/org/ulman/simulator/ui/SimulatorIJ2UI.java
@@ -3,6 +3,7 @@ package org.ulman.simulator.ui;
 import net.imagej.ImageJ;
 import org.mastodon.mamut.MainWindow;
 import org.mastodon.mamut.ProjectModel;
+import org.mastodon.mamut.io.ProjectSaver;
 import org.mastodon.mamut.io.project.MamutProject;
 import org.mastodon.mamut.model.Model;
 import org.mastodon.views.bdv.SharedBigDataViewerData;
@@ -11,6 +12,8 @@ import org.scijava.plugin.Parameter;
 import org.scijava.plugin.Plugin;
 import org.ulman.simulator.Simulator;
 import javax.swing.WindowConstants;
+import java.io.File;
+import java.io.IOException;
 
 
 @Plugin(type = Command.class)
@@ -44,18 +47,46 @@ public class SimulatorIJ2UI implements Command {
 		}
 	}
 
+	// ===============================================================================================
 	public static void main(String[] args) {
+		runWithGUI();
+		//runHeadless("/temp/CLsim.mastodon");
+	}
+
+	public static void runWithGUI() {
 		System.setProperty( "apple.laf.useScreenMenuBar", "true" );
 
 		ImageJ ij = new ImageJ();
 		ij.ui().showUI();
 
-		final ProjectModel appModel = ProjectModel.create(ij.getContext(),
+		final ProjectModel projectModel = ProjectModel.create(ij.getContext(),
 				new Model(),
 				SharedBigDataViewerData.fromDummyFilename("DUMMY x=100 y=100 z=100 t=100.dummy"),
 				new MamutProject("/temp/CLsim.mastodon"));
-		final MainWindow win = new MainWindow( appModel );
+		final MainWindow win = new MainWindow( projectModel );
 		win.setVisible( true );
 		win.setDefaultCloseOperation( WindowConstants.EXIT_ON_CLOSE );
+	}
+
+	public static void runHeadless(final String projectFileName) {
+		final String DUMMYXML="DUMMY x=100 y=100 z=100 t=100.dummy";
+		ImageJ ij = new ImageJ();
+		final ProjectModel projectModel = ProjectModel.create(ij.getContext(),
+				new Model(),
+				SharedBigDataViewerData.fromDummyFilename(DUMMYXML),
+				new MamutProject(projectFileName));
+
+		SimulatorIJ2UI thisFrame = new SimulatorIJ2UI();
+		thisFrame.projectModel = projectModel;
+		thisFrame.run();
+
+		try {
+			projectModel.getProject().setDatasetXmlFile(new File(DUMMYXML));
+			ProjectSaver.saveProject(new File(projectFileName), projectModel);
+			System.out.println("PROJECT SAVED TO "+projectFileName);
+		} catch (IOException e) {
+			System.out.println("Hmm... some issue saving the file "+projectFileName);
+			System.out.println("Complaint was: "+e.getMessage());
+		}
 	}
 }
