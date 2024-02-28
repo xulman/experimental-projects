b0d9befdb90868d90b309cd488f78f4182548f0d
Vladimír Ulman <ulman@fi.muni.cz>
2024-02-28 15:51:22 +0100

    DEL,CHG: Simulator removed the old reporting into a file...

--- mastodon/src/main/java/org/ulman/simulator/Simulator.java
+++ mastodon/src/main/java/org/ulman/simulator/Simulator.java
@@ -10,10 +10,11 @@ public class Simulator {
 	private final Map<Integer, Agent> agentsContainer = new HashMap<>(10000);
 	private final List<Agent> newAgentsContainer = new ArrayList<>(100);
 	private final List<Agent> deadAgentsContainer = new ArrayList<>(100);
-	private final PrintWriter reportFile;
 
-	public Simulator(String logFilePath) throws FileNotFoundException {
-		this.reportFile = new PrintWriter(logFilePath);
+	private final ProjectModel projectModel;
+
+	public Simulator(final ProjectModel projectModel) throws FileNotFoundException {
+		this.projectModel = projectModel;
 	}
 
 	synchronized
@@ -45,20 +46,11 @@ public class Simulator {
 	public void commitNewAndDeadAgents() {
 		for (Agent spot : this.deadAgentsContainer) {
 			this.agentsContainer.remove(spot.getId());
-			this.reportAgentLog(spot);
 		}
 
 		for (Agent spot : this.newAgentsContainer) {
 			this.agentsContainer.put(spot.getId(), spot);
 		}
-	}
-
-	public void reportAgentLog(Agent spot) {
-		for (String line : spot.getReportLog()) {
-			reportFile.println(line);
-		}
-		reportFile.println();
-		reportFile.println();
 	}
 
 	public List<double[]> getListOfOccupiedCoords(Agent fromThisSpot) {
@@ -106,19 +98,5 @@ public class Simulator {
 	}
 
 	public void close() {
-		agentsContainer.forEach( (id,spot) -> reportAgentLog(spot) );
-		reportFile.close();
-	}
-
-
-	public static void main(String[] args) {
-		try {
-			Simulator s = new Simulator("/temp/mastodon_plain_text_log1.txt");
-			s.populate(2);
-			for (int time = 0; time < 10; ++time) s.doOneTime();
-			s.close();
-		} catch (Exception e) {
-			System.out.println("SOME ERROR: "+e.getMessage());
-		}
 	}
 }
