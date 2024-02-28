b22dbbbed0599abd5395e608480e2720f6313a74
Vladimír Ulman <ulman@fi.muni.cz>
2024-02-28 12:18:16 +0100

    ADD: org.ulman.simulator.Agent and Simulator from their Python counterparts:
    translated by ChatGPT, and post-edited (lightly...)

---
+++ mastodon/src/main/java/org/ulman/simulator/Agent.java
@@ -0,0 +1,189 @@
+package org.ulman.simulator;
+
+import java.util.*;
+
+public class Agent {
+	private final Simulator simulatorFrame;
+
+	private String name;
+	private final String nameClean;
+	private final String nameBlocked;
+	private final String nameWantDivide;
+	private final String nameBlockedWantDivide;
+
+	private final int id;
+	public int getId() {
+		return id;
+	}
+
+	private final int parentId;
+
+	private int t;
+	private double x,y,z;
+	private double nextX,nextY,nextZ;
+	private final double interestRadius = 5.0;
+
+	public double getX() { return x; }
+	public double getY() { return y; }
+	public double getZ() { return z; }
+	public double getInterestRadius() { return interestRadius; }
+
+	private final double usualStepSize = 1.0;
+	private final double minDistanceToNeighbor = 3.0 * usualStepSize;
+
+	private final int dontDivideBefore;
+	private final int dontLiveBeyond;
+	private final int maxNeighborsForDivide = 4;
+
+	private final List<String> reportLog = new ArrayList<>(100);
+	public List<String> getReportLog() {
+		return reportLog;
+	}
+
+
+	public Agent(Simulator simulator, int ID, int parentID, String label, double x, double y, double z, int time) {
+		this.simulatorFrame = simulator;
+
+		this.name = label;
+		this.nameClean = label;
+		this.nameBlocked = "B" + label;
+		this.nameWantDivide = "W" + label;
+		this.nameBlockedWantDivide = "BW" + label;
+
+		this.id = ID;
+		this.parentId = parentID;
+		this.t = time;
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		this.nextX = x;
+		this.nextY = y;
+		this.nextZ = z;
+
+		double meanLifePeriod = 7;
+		double sigma = (0.6 * meanLifePeriod) / 3.0;
+		double thisCellLifePeriod = new Random().nextGaussian() * sigma + meanLifePeriod;
+
+		this.dontDivideBefore = time + (int) thisCellLifePeriod;
+		this.dontLiveBeyond = time + 5 * (int) thisCellLifePeriod;
+
+		if (parentID == 0) {
+			this.reportStatus();
+		}
+
+		System.out.printf("NEW AGENT %d (%s), parent %d @ [%f,%f,%f] tp=%d, divTime=%d, dieTime=%d%n", ID, label, parentID, x, y, z, time, this.dontDivideBefore, this.dontLiveBeyond);
+	}
+
+	public void reportStatus() {
+		this.reportLog.add(String.format("%d\t%f\t%f\t%f\t%d\t%d\t%s", this.t, this.x, this.y, this.z, this.id, this.parentId, this.name));
+	}
+
+	public void progress(int tillThisTime) {
+		boolean firstGo = true;
+		while (this.t < tillThisTime) {
+			this.doOneTime(firstGo);
+			firstGo = false;
+		}
+	}
+
+	public void progressFinish() {
+		this.x = this.nextX;
+		this.y = this.nextY;
+		this.z = this.nextZ;
+		this.reportStatus();
+	}
+
+	public void doOneTime(boolean fromCurrentPos) {
+		System.out.printf("advancing agent id %d (%s):%n", this.id, this.name);
+
+		double oldX = fromCurrentPos ? this.x : this.nextX;
+		double oldY = fromCurrentPos ? this.y : this.nextY;
+		double oldZ = fromCurrentPos ? this.z : this.nextZ;
+		System.out.printf("  from pos [%f,%f,%f] (from_current_pos=%b)%n", oldX, oldY, oldZ, fromCurrentPos);
+
+		List<double[]> neighbors = simulatorFrame.getListOfOccupiedCoords(this);
+		System.out.println("	neighs: " + neighbors);
+
+		final double minDistanceSquared = minDistanceToNeighbor * minDistanceToNeighbor;
+		int doneAttempts = 0;
+		boolean tooClose = true;
+		double dispX = 0;
+		double dispY = 0;
+		while (doneAttempts < 5 && tooClose) {
+			doneAttempts += 1;
+
+			boolean isOdd = (doneAttempts & 1) == 1;
+			if (isOdd) {
+				dispX = new Random().nextGaussian() * (this.usualStepSize / 2.0);
+				dispY = new Random().nextGaussian() * (this.usualStepSize / 2.0);
+			} else {
+				dispX /= 2.0;
+				dispY /= 2.0;
+			}
+			System.out.printf("  displacement = (%f,%f), isOdd=%b%n", dispX, dispY, isOdd);
+
+			double newX = oldX + dispX;
+			double newY = oldY + dispY;
+			double newZ = oldZ;
+
+			tooClose = false;
+			for (double[] n : neighbors) {
+				double dx = n[0] - newX;
+				double dy = n[1] - newY;
+				double dz = n[2] - newZ;
+				double dist = dx * dx + dy * dy + dz * dz;
+				if (dist < minDistanceSquared) {
+					tooClose = true;
+					break;
+				}
+			}
+			System.out.printf("  trying pos [%f,%f,%f], too_close=%b%n", newX, newY, newZ, tooClose);
+
+			if (!tooClose) {
+				this.nextX = newX;
+				this.nextY = newY;
+				this.nextZ = newZ;
+				this.name = this.nameClean;
+			} else {
+				System.out.println("	couldn't move when " + neighbors.size() + " neighbors are around");
+				this.name = this.nameBlocked;
+			}
+			this.t += 1;
+
+			System.out.printf("  established coords [%f,%f,%f] (required %d attempts)%n", this.nextX, this.nextY, this.nextZ, doneAttempts);
+			System.out.printf("  when %d neighbors around, too_close=%b%n", neighbors.size(), tooClose);
+
+			if (this.t > this.dontLiveBeyond) {
+				System.out.println("  dying now!");
+				this.simulatorFrame.deregisterAgent(this);
+			} else if (this.t > this.dontDivideBefore) {
+				int noOfNeighbors = neighbors.size();
+				if (noOfNeighbors <= this.maxNeighborsForDivide && !tooClose) {
+					System.out.println("  dividing!");
+					this.divideMe();
+				} else {
+					System.out.printf("  should divide but space seems to be full... (%d neighbors, too_close=%b)%n", noOfNeighbors, tooClose);
+					this.name = tooClose ? this.nameBlockedWantDivide : this.nameWantDivide;
+				}
+			}
+		}
+	}
+
+	public void divideMe() {
+		int d1Id = this.simulatorFrame.getNewId();
+		int d2Id = this.simulatorFrame.getNewId();
+
+		String d1Name = this.name + "a";
+		String d2Name = this.name + "b";
+
+		double alfa = new Random().nextDouble() * 6.28;
+		double dx = 0.5 * this.minDistanceToNeighbor * Math.cos(alfa);
+		double dy = 0.5 * this.minDistanceToNeighbor * Math.sin(alfa);
+		Agent d1 = new Agent(this.simulatorFrame, d1Id, this.id, d1Name, this.nextX - dx, this.nextY - dy, this.nextZ, this.t + 1);
+		Agent d2 = new Agent(this.simulatorFrame, d2Id, this.id, d2Name, this.nextX + dx, this.nextY + dy, this.nextZ, this.t + 1);
+
+		this.simulatorFrame.deregisterAgent(this);
+		this.simulatorFrame.registerAgent(d1);
+		this.simulatorFrame.registerAgent(d2);
+	}
+}
---
+++ mastodon/src/main/java/org/ulman/simulator/Simulator.java
@@ -0,0 +1,112 @@
+package org.ulman.simulator;
+
+import java.io.*;
+import java.util.*;
+
+
+public class Simulator {
+	private int assignedIds = 0;
+	private int time = 0;
+	private final Map<Integer, Agent> agentsContainer = new HashMap<>(10000);
+	private final List<Agent> newAgentsContainer = new ArrayList<>(100);
+	private final List<Agent> deadAgentsContainer = new ArrayList<>(100);
+	private final PrintWriter reportFile;
+
+	public Simulator(String logFilePath) throws FileNotFoundException {
+		this.reportFile = new PrintWriter(logFilePath);
+	}
+
+	synchronized
+	public int getNewId() {
+		this.assignedIds += 1;
+		return this.assignedIds;
+	}
+
+	synchronized
+	public void registerAgent(Agent spot) {
+		if (this.agentsContainer.containsKey(spot.getId())) {
+			System.out.println("========== SIM: ERROR with registering");
+			return;
+		}
+		System.out.println("========== SIM: registering agent " + spot.getId());
+		this.newAgentsContainer.add(spot);
+	}
+
+	synchronized
+	public void deregisterAgent(Agent spot) {
+		if (!this.agentsContainer.containsKey(spot.getId())) {
+			System.out.println("========== SIM: ERROR with deregistering");
+			return;
+		}
+		System.out.println("========== SIM: DEregistering agent " + spot.getId());
+		this.deadAgentsContainer.add(spot);
+	}
+
+	public void commitNewAndDeadAgents() {
+		for (Agent spot : this.deadAgentsContainer) {
+			this.agentsContainer.remove(spot.getId());
+			this.reportAgentLog(spot);
+		}
+
+		for (Agent spot : this.newAgentsContainer) {
+			this.agentsContainer.put(spot.getId(), spot);
+		}
+	}
+
+	public void reportAgentLog(Agent spot) {
+		for (String line : spot.getReportLog()) {
+			reportFile.println(line);
+		}
+		reportFile.println();
+		reportFile.println();
+	}
+
+	public List<double[]> getListOfOccupiedCoords(Agent fromThisSpot) {
+		final double interestRadius = fromThisSpot.getInterestRadius();
+		final double minX = fromThisSpot.getX() - interestRadius;
+		final double minY = fromThisSpot.getY() - interestRadius;
+		final double minZ = fromThisSpot.getZ() - interestRadius;
+
+		final double maxX = fromThisSpot.getX() + interestRadius;
+		final double maxY = fromThisSpot.getY() + interestRadius;
+		final double maxZ = fromThisSpot.getZ() + interestRadius;
+
+		final List<double[]> retCoords = new ArrayList<>(100);
+		for (Agent spot : agentsContainer.values()) {
+			if (spot.getId() == fromThisSpot.getId()) {
+				continue;
+			}
+			if (minX < spot.getX() && spot.getX() < maxX
+					&& minY < spot.getY() && spot.getY() < maxY
+					&& minZ < spot.getZ() && spot.getZ() < maxZ) {
+				retCoords.add(new double[]{spot.getX(), spot.getY(), spot.getZ()});
+			}
+		}
+
+		return retCoords;
+	}
+
+	public void doOneTime() {
+		newAgentsContainer.clear();
+		deadAgentsContainer.clear();
+
+		time += 1;
+		System.out.println("========== SIM: creating time point " + time + " from " + agentsContainer.size() + " agents");
+		agentsContainer.forEach( (id,spot) -> spot.progress(time) );
+		agentsContainer.forEach( (id,spot) -> spot.progressFinish() );
+		commitNewAndDeadAgents();
+	}
+
+	public void populate(int numberOfCells) {
+		for (int i = 0; i < numberOfCells; i++) {
+			Agent spot = new Agent(this, this.getNewId(), 0, String.valueOf(i + 1), i * 3, 0, 0, this.time);
+			this.registerAgent(spot);
+		}
+		this.commitNewAndDeadAgents();
+	}
+
+	public void close() {
+		agentsContainer.forEach( (id,spot) -> reportAgentLog(spot) );
+		reportFile.close();
+	}
+}
