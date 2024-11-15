9681051acd53eb063dee133ff7e7aa14f44af541
Vladimír Ulman <ulman@fi.muni.cz>
2024-11-15 22:28:20 +0100

    CHG2/3: BenchmarkMeasurement.stats methods use streamOfValidOnly(), adapted for null measurements

--- mastodon/src/main/java/org/mastodon/benchmark/measurements/BenchmarkMeasurement.java
+++ mastodon/src/main/java/org/mastodon/benchmark/measurements/BenchmarkMeasurement.java
@@ -3,6 +3,7 @@ package org.mastodon.benchmark.measurements;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -32,28 +33,32 @@ public class BenchmarkMeasurement {
 	}
 
 	public double getSum() {
-		return measuredTimes.stream().reduce(0.0, Double::sum);
+		return streamOfValidOnly().reduce(0.0, Double::sum);
 	}
 
 	public double getMin() {
-		if (measuredTimes.isEmpty()) return 0;
-		return measuredTimes.stream().min(Double::compareTo).get();
+		Optional<Double> m = streamOfValidOnly().min(Double::compareTo);
+		return m.isPresent() ? m.get() : 0;
 	}
 
 	public double getMax() {
-		if (measuredTimes.isEmpty()) return 0;
-		return measuredTimes.stream().max(Double::compareTo).get();
+		Optional<Double> m = streamOfValidOnly().max(Double::compareTo);
+		return m.isPresent() ? m.get() : 0;
 	}
 
 	public double getAvg() {
-		if (measuredTimes.isEmpty()) return 0;
-		double sum = measuredTimes.stream().reduce(0.0, Double::sum);
-		sum /= measuredTimes.size();
+		int size = numOfValidOnly();
+		if (size == 0) return 0;
+
+		double sum = streamOfValidOnly().reduce(0.0, Double::sum);
+		sum /= size;
 		return sum;
 	}
 
 	public double getMedian() {
-		if (measuredTimes.isEmpty()) return 0;
-		return measuredTimes.stream().sorted().collect(Collectors.toList()).get(measuredTimes.size() / 2);
+		int size = numOfValidOnly();
+		if (size == 0) return 0;
+
+		return streamOfValidOnly().sorted().collect(Collectors.toList()).get(size / 2);
 	}
 }
