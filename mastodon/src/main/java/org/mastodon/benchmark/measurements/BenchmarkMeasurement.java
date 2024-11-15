d361dfac80ce3032bb4bf0cd4210b2b48123f6fd
Vladimír Ulman <ulman@fi.muni.cz>
2024-11-15 17:57:32 +0100

    BUGFIX: BenchmarkMeasurement stats on empty records were failing (or doing NaNs)

--- mastodon/src/main/java/org/mastodon/benchmark/measurements/BenchmarkMeasurement.java
+++ mastodon/src/main/java/org/mastodon/benchmark/measurements/BenchmarkMeasurement.java
@@ -15,20 +15,24 @@ public class BenchmarkMeasurement {
 	}
 
 	public double getMin() {
+		if (measuredTimes.isEmpty()) return 0;
 		return measuredTimes.stream().min(Double::compareTo).get();
 	}
 
 	public double getMax() {
+		if (measuredTimes.isEmpty()) return 0;
 		return measuredTimes.stream().max(Double::compareTo).get();
 	}
 
 	public double getAvg() {
+		if (measuredTimes.isEmpty()) return 0;
 		double sum = measuredTimes.stream().reduce(0.0, Double::sum);
 		sum /= measuredTimes.size();
 		return sum;
 	}
 
 	public double getMedian() {
+		if (measuredTimes.isEmpty()) return 0;
 		return measuredTimes.stream().sorted().collect(Collectors.toList()).get(measuredTimes.size() / 2);
 	}
 }
