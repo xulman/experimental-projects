55735a445ddf0829763ea8b0f445b1d550b09d71
Vladimír Ulman <ulman@fi.muni.cz>
2024-11-22 20:26:20 +0100

    ADD: plot_as_bar_plot(), and plot() renamed to plot_as_individual_lines(), clean ups (small)

--- python/benchmark/create_plots.py
+++ python/benchmark/create_plots.py
@@ -15,15 +15,33 @@ def list_columns_with_data(table) -> list[str]:
     return [ c for i,c in enumerate(table.columns) if i > 7 ]
 
 
-data = pd.read_csv(csv_file, delimiter='\t')
-x_labels = list_columns_with_data(data)
+csv_table = pd.read_csv(csv_file, delimiter='\t')
+x_labels = list_columns_with_data(csv_table)
 
-def plot(table, x_labels, source:str):
+def plot_as_individual_lines(table, x_labels, source:str):
     x_tics = range(len(x_labels))
-    for row_idx in list_rows_with_source(data,source):
-        y_values = [ data[col_label][row_idx] for col_label in x_labels ]
-        #plt.plot(x_labels,y_values,'lo')
-        plt.plot(x_tics,y_values,'-o')
+    for row_idx in list_rows_with_source(table,source):
+        y_values = [ table[col_label][row_idx] for col_label in x_labels ]
+        #plt.plot(x_labels,y_values,'-o')
+        line_label = source+str(table['round'][row_idx])
+        plt.plot(x_tics,y_values,'-o',markersize=3,label=line_label)
+
+
+def plot_as_bar_plot(table, x_labels, source:str):
+    rows_idxs = list_rows_with_source(table,source)
+    vals = np.zeros([len(x_labels),len(rows_idxs)], dtype=float) # [commands, rounds]
+    #
+    for ri,row_idx in enumerate(rows_idxs):
+        for ci,col_label in enumerate(x_labels):
+            vals[ci,ri] = table[col_label][row_idx]
+    #
+    stats = np.zeros([len(x_labels),2], dtype=float) # [commands, 2 for mean and std]
+    for i in range(stats.shape[0]):
+        stats[i,0] = vals[i].mean()
+        stats[i,1] = vals[i].std()
+    #
+    x_tics = range(len(x_labels))
+    plt.errorbar(x_tics,stats[:,0], yerr=stats[:,1])
 
 
 # ----------------------------------------------------------------------
